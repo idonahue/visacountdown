f9a76744bd3a5a1954ad0972ed94660e64f6c35f
Fabien Loup <info@norydev.com>
2015-10-23 15:30:28 +0300

    added UAE

--- config/initializers/countries.rb
+++ config/initializers/countries.rb
@@ -1,6 +1,6 @@
-COUNTRY_LIST = YAML.load_file("#{Rails.root}/config/countries.yml")
+# COUNTRY_LIST = YAML.load_file("#{Rails.root}/config/countries.yml")
 POLICIES = YAML.load_file("#{Rails.root}/config/policies.yml")
 
-COUNTRIES = (["None"] << POLICIES["Schengen area"]["policies"].keys.sort.reject!{|k| k == "None"}).flatten
+COUNTRIES = (["None"] + POLICIES["Schengen area"]["policies"].keys.sort - ["None"])
 SCHENGEN = %w(Austria Belgium Czech\ Republic Denmark Estonia Finland France Germany Greece Hungary Iceland Italy Latvia Lithuania Luxembourg Malta Netherlands Norway Poland Portugal Slovakia Slovenia Spain Sweden Switzerland Liechtenstein)
 ZONES = POLICIES.keys.sort
